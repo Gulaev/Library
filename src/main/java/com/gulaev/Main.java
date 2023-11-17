0653b5cac04ee81f762602d2372a7a1d42c330cf
Denis Gulaev <dennysittohero@gmail.com>
2023-11-17 20:51:38 +0200

    Fix some litle bags

--- src/main/java/com/gulaev/Main.java
+++ src/main/java/com/gulaev/Main.java
@@ -10,37 +10,31 @@ import com.gulaev.service.UserService;
 import com.gulaev.user.LibraryUser;
 import java.util.Arrays;
 import java.util.Set;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 
 public class Main {
 
-  private static final Logger LOG = LogManager.getLogger(Main.class);
-
   public static void main(String[] args) {
 
-    BookItem bookItem1 = BookService.createNewBookItem("Nick", "Descriptiojkahfkajhfkjahasflkasjflkajsflkasjflkjaslkfjaslkfjlasfkashfkahsfkjhaskfhasn1", "Book", 10,
-        Genre.FANTASY, Arrays.asList(Tag.WAR, Tag.COMEDY));
+    BookItem bookItem1 = BookService.createNewBookItem("Nick",
+        "An intriguing novel by Nick, delving into the complexities of human nature and the relentless pursuit of truth. With eloquent prose and engaging storytelling, this book captivates readers with its rich narrative and thought-provoking themes. Set in a world filled with suspense and mystery, the story unfolds, enthralling readers page by page.",
+        "Book", 10, Genre.FANTASY, Arrays.asList(Tag.WAR, Tag.COMEDY));
     BookItem bookItem2 = new BookItem(2, "Title2", "John", "Description2", 20,
         Arrays.asList(Tag.HISTORY, Tag.WAR), Genre.MYSTERY);
     BookItem bookItem3 = new BookItem(3, "Title3", "Nick", "Description3", 30,
         Arrays.asList(Tag.COMEDY, Tag.HISTORY), Genre.MYSTERY);
 
-    OnlineBook onlineBook = OnlineBook.builder()
-        .setTitle("Title")
-        .setId(1).setAuthor("Nithe").setCover("not path")
-        .setDescription("Description").setGenre(Genre.FANTASY)
-        .setPrice(300).setTags(Arrays.asList(Tag.DRAMA, Tag.HISTORY)).build();
+    OnlineBook onlineBook = OnlineBook.builder().setTitle("Title").setId(1).setAuthor("Nithe")
+        .setCover("not path").setDescription("Description").setGenre(Genre.FANTASY).setPrice(300)
+        .setTags(Arrays.asList(Tag.DRAMA, Tag.HISTORY)).build();
 
-
-    LibraryUser user =
-        UserService.createNewLibraryUser("suser", "email@email.com", "John", "Doe", "Abc1234");
+    LibraryUser user = UserService.createNewLibraryUser("suser", "email@email.com", "John", "Doe",
+        "Abc1234");
 
     user.setWishlist(Arrays.asList(bookItem1, bookItem2));
     user.setBoughtBooks(Arrays.asList(bookItem3));
 
     user = UserService.changePasswordToUser(user, "Abc1234", "Abc1235");
-
 
     Set<BookItem> recommendedBookItems = BookRecommendationService.findBookByPreferences(user);
     System.out.println(onlineBook.toString());
--- src/main/java/com/gulaev/exception/PasswordDidntMatchException.java
+++ src/main/java/com/gulaev/exception/PasswordDidntMatchException.java
@@ -1,6 +1,6 @@
 package com.gulaev.exception;
 
-public class PasswordDidntMatchException extends Exception{
+public class PasswordDidntMatchException extends Exception {
 
   public PasswordDidntMatchException(String message) {
     super(message);
